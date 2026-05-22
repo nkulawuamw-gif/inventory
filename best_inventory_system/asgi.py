e3af9742a440492de8c8c70c00147bb4fb5c154d
nkulawuamw-gif <nkulawuamw-gif@users.noreply.github.com>
2026-05-23 00:04:23 +0200

    fix: clean asgi.py - call get_asgi_application before channels imports to avoid circular import

--- best_inventory_system/asgi.py
+++ best_inventory_system/asgi.py
@@ -1,17 +1,16 @@
 import os
-import django
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "best_inventory_system.settings")
 
-django.setup()
+django_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
 from stock_manager.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
