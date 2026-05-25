20746054a73ef3aa5ca80b7b44d6e6abffe70667
Mantchombe <mantchombe@users.noreply.github.com>
2026-05-25 12:38:27 +0200

    fix: remove module-level get_user_model() call that crashed daphne on startup

--- stock_manager/consumers.py
+++ stock_manager/consumers.py
@@ -1,8 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from django.contrib.auth import get_user_model
-User = get_user_model()
 from django.utils import timezone
 from .models import Message, Call, UserPresence
 
