4aba5dd8389ec33410333c098b061d187e0cbc9d
Mantchombe <mantchombe@users.noreply.github.com>
2026-05-25 14:35:33 +0200

    add static file serving via url patterns for production

--- best_inventory_system/urls.py
+++ best_inventory_system/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
+from django.contrib.staticfiles.views import serve as staticfiles_serve
 from django.http import JsonResponse
 from django.views.generic.base import RedirectView
 
@@ -22,3 +23,7 @@ else:
     urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     ]
+
+urlpatterns += [
+    re_path(r'^static/(?P<path>.*)$', staticfiles_serve, {'insecure': True}),
+]
